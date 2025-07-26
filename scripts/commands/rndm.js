9f6502232e87125e1ad51b2236dbf0ab2f304508
MOHAMMAD RAHAT <fmznobita3@gmail.com>
2025-07-27 03:20:32 +0600

    Update rndm.js

--- scripts/commands/rndm.js
+++ scripts/commands/rndm.js
@@ -31,7 +31,7 @@ module.exports.run = async function ({ api, event }) {
       .pipe(file)
       .on("close", () => {
         return api.sendMessage({
-          body: `${cp}\n\n𝐓𝐨𝐭𝐚𝐥 𝐕𝐢𝐝𝐞𝐨𝐬: [${ln}]\n𝐀𝐝𝐝𝐞𝐝 𝐓𝐡𝐢𝐬 𝐕𝐢𝐝𝐞𝐨 𝐓𝐨 𝐓𝐡𝐞 𝐀𝐩𝐢 𝐁𝐲 [${name}]`,
+          body: `${cp}\n\n𝐓𝐨𝐭𝐚𝐥 𝐕𝐢𝐝𝐞𝐨𝐬: [${ln}]\n𝐀𝐝𝐝𝐞𝐝 𝐓𝐡𝐢𝐬 𝐕𝐢𝐝𝐞𝐨 𝐓𝐨 𝐓𝐡𝐞 𝐀𝐩𝐢 𝐁𝐲 [𝙼𝙾𝙷𝙰𝙼𝙼𝙰𝙳 𝚁𝙰𝙷𝙰𝚃]`,
           attachment: fs.createReadStream(filePath)
         }, event.threadID, event.messageID);
       });
